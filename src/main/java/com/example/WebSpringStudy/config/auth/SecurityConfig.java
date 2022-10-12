45fa4127f8f61707913593046572338f75734371
WooHCode <gusdn526@naver.com>
2022-10-12 22:32:58 +0900

    Initial commit

---
+++ src/main/java/com/example/WebSpringStudy/config/auth/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.WebSpringStudy.config.auth;
+
+import com.example.WebSpringStudy.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity  //Spring Security 기능 활성화
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                //h2-console 화면을 사용하기 위한 disable
+                .csrf().disable().headers().frameOptions().disable()
+                .and()
+                    //열람 권한이 있는 url 지정
+                    .authorizeRequests()
+                        .antMatchers("/","/css/**","/images/**","/js/**","/h2-console/**")
+                    .permitAll()
+                        //api/v1/**의 api는 USER권한을 가진 사람만 사용 가능
+                        .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                            .anyRequest().authenticated()
+                .and()
+                    .logout().logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                    .userInfoEndpoint()
+                    .userService(customOAuth2UserService);
+    }
+}
---
+++ src/test/java/com/example/WebSpringStudy/WebSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WebSpringStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
